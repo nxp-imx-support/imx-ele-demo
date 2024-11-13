66bcd9fc6b7e4674fc1ec560c890c57cfaaa5c5a
yanxing cui <yanxing.cui@nxp.com>
2024-11-13 11:10:48 +0800

    [MICRSE-2996] switch license to LA_OPT_Online Code Hosting
    Signed-off-by: yanxing cui <yanxing.cui@nxp.com>

--- ele/src/read_rng.c
+++ ele/src/read_rng.c
@@ -1,6 +1,6 @@
 /*
 * Copyright 2024 NXP
-* NXP Confidential and Proprietary. This software is owned or controlled by NXP and may only be used strictly in
+* NXP Proprietary. This software is owned or controlled by NXP and may only be used strictly in
 * accordance with the applicable license terms. By expressly accepting such terms or by downloading, installing,
 * activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
 * comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license
