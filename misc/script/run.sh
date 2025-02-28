2134feeef0c7a89b02664c97b5083c6a47094b85
yanxing cui <yanxing.cui@nxp.com>
2025-02-28 11:26:42 +0800

    add sleep after nvmdaemon
    Signed-off-by: yanxing cui <yanxing.cui@nxp.com>

--- misc/script/run.sh
+++ misc/script/run.sh
@@ -5,9 +5,10 @@
 # activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to
 # comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license
 # terms, then you may not retain, install, activate or otherwise use the software.
-# 
+#
 
 systemctl start nvm_daemon
+sleep 2
 #make sure the executable eledemo and run.sh are in the same folder
 DIR=$(dirname $0)
 $DIR/eledemo
